2611d6c0ad4c15c6b85aae9fb0fb0b11964a1f0c
madmadi <mo@madmadi.me>
2021-05-18 12:11:52 +0430

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,18 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: isProd ? 'production' : 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
   plugins: [
     new HTMLWebpackPlugin({
       template: 'src/index.html',
